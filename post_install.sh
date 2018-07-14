029d4aa9fbbad473810a9dde3a81db34abf134b6
Shank <shashanksharma21@gmail.com>
2018-07-14 21:01:52 +0530

    fixed the vscode extension installation issue

--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,15 @@
 EXTENSIONS=(
-    "felipecaputo.git-project-manager",
-    "Gruntfuggly.activitusbar",
-    "JakeBecker.elixir-ls",
-    "ms-python.python",
-    "ms-vscode.sublime-keybindings",
-    "PeterJausovec.vscode-docker",
-    "tomphilbin.gruvbox-themes",
+    "felipecaputo.git-project-manager"
+    "Gruntfuggly.activitusbar"
+    "JakeBecker.elixir-ls"
+    "ms-python.python"
+    "ms-vscode.sublime-keybindings"
+    "PeterJausovec.vscode-docker"
+    "tomphilbin.gruvbox-themes"
     "vscoss.vscode-ansible"
 )
 
-for extension in $EXTENSIONS; do
-    code --install-extension $extension
+for index in ${!EXTENSIONS[@]}; do
+    echo "installing vscode extension: ${EXTENSIONS[$index]}"
+    code --install-extension ${EXTENSIONS[$index]}
 done
