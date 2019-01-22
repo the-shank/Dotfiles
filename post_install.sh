97544589a41ff94e731e3fba744c55aa0a19bbd0
Shank <shashanksharma21@gmail.com>
2019-01-22 14:17:22 +0530

    improve the script to install the required vscode extensions

--- post_install.sh
+++ post_install.sh
@@ -1,20 +1,52 @@
-EXTENSIONS=(
-    "felipecaputo.git-project-manager"
-    "Gruntfuggly.activitusbar"
-    "JakeBecker.elixir-ls"
-    "ms-python.python"
-    "ms-vscode.sublime-keybindings"
-    "PeterJausovec.vscode-docker"
-    "jdinhlife.gruvbox"
-    "vscoss.vscode-ansible"
-    "bungcip.better-toml"
-    "hangxingliu.vscode-nginx-conf-hint"
-    "burkeholland.simple-timer"
-    "octref.vetur"
-    "karyfoundation.theme-karyfoundation-themes"
-)
+# ========= UTILS ===================
 
-for index in ${!EXTENSIONS[@]}; do
-    echo "installing vscode extension: ${EXTENSIONS[$index]}"
-    code --install-extension ${EXTENSIONS[$index]}
-done
+# contains arr value
+function contains() 
+{
+    local array="$1[@]"
+    local seeking=$2
+    local notfound=1
+    for element in ${!array}; do
+        if [[ $element == $seeking ]]; then
+            notfound=0
+            break
+        fi
+    done
+    return $notfound
+}
+
+# =====================================
+
+function vscode_setup()
+{
+    RequiredExtensions=(
+        "Gruntfuggly.activitusbar"          # Activitus Bar
+        "vscoss.vscode-ansible"             # Ansible
+        "bungcip.better-toml"               # Better TOML
+        "PeterJausovec.vscode-docker"       # Docker
+        "JakeBecker.elixir-ls"              # ElixirLS
+        "felipecaputo.git-project-manager"  # Git Project Manager
+        "jdinhlife.gruvbox"                 # Gruvbox Theme
+        "karyfoundation.theme-karyfoundation-themes"    # Kary Pro Colors
+        "hangxingliu.vscode-nginx-conf-hint"            # nginx.conf hint
+        "passionkind.prettier-vscode-with-tabs"         # Prettier - JavaScript Formatter
+        "ms-python.python"                  # Python
+        "burkeholland.simple-timer"         # Simple Timer
+        "ms-vscode.sublime-keybindings"     # Sublime Text Keymap and Settings
+        "octref.vetur"                      # Vetur
+    )
+
+    mapfile -t InstalledExtensions < <( code --list-extensions )
+    echo "Installing VSCode Extensions:"
+    for extn in ${RequiredExtensions[@]}; do
+        contains InstalledExtensions $extn && present="yes" || present="no"
+        if [[ $present == "yes" ]]; then
+            echo "$extn already installed, skipping..."
+        elif [[ $present == "no" ]]; then
+            echo "installing vscode extension: ${extn}"
+            code --install-extension ${extn}
+        fi
+    done
+}
+
+vscode_setup
