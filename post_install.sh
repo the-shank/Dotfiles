2f4bc69ec58211e1de3a805ae41cb99e34228469
Shank <shashanksharma21@gmail.com>
2019-02-25 00:27:13 +0530

    improve vscode extn installation

--- post_install.sh
+++ post_install.sh
@@ -19,33 +19,35 @@ function contains()
 
 function vscode_setup()
 {
-    RequiredExtensions=(
-        "Gruntfuggly.activitusbar"          # Activitus Bar
-        "vscoss.vscode-ansible"             # Ansible
-        "bungcip.better-toml"               # Better TOML
-        "PeterJausovec.vscode-docker"       # Docker
-        "JakeBecker.elixir-ls"              # ElixirLS
-        "pgourlain.erlang"                  # Erlang
-        "sleistner.vscode-fileutils"        # File Utils
-        "felipecaputo.git-project-manager"  # Git Project Manager
-        "jdinhlife.gruvbox"                 # Gruvbox Theme
-        "karyfoundation.theme-karyfoundation-themes"    # Kary Pro Colors
-        "hangxingliu.vscode-nginx-conf-hint"            # nginx.conf hint
-        "passionkind.prettier-vscode-with-tabs"         # Prettier - JavaScript Formatter
-        "ms-python.python"                  # Python
-        "burkeholland.simple-timer"         # Simple Timer
-        "ms-vscode.sublime-keybindings"     # Sublime Text Keymap and Settings
-        "octref.vetur"                      # Vetur
-    )
+    echo "Installing VSCode extensions"
 
-    mapfile -t InstalledExtensions < <( code --list-extensions )
-    echo "Installing VSCode Extensions:"
+    InstalledExtensions=$(code --list-extensions)
+
+    RequiredExtensions=(
+            "Gruntfuggly.activitusbar"          # Activitus Bar
+            "vscoss.vscode-ansible"             # Ansible
+            "bungcip.better-toml"               # Better TOML
+            "PeterJausovec.vscode-docker"       # Docker
+            "JakeBecker.elixir-ls"              # ElixirLS
+            "pgourlain.erlang"                  # Erlang
+            "sleistner.vscode-fileutils"        # File Utils
+            "felipecaputo.git-project-manager"  # Git Project Manager
+            "jdinhlife.gruvbox"                 # Gruvbox Theme
+            "karyfoundation.theme-karyfoundation-themes"    # Kary Pro Colors
+            "hangxingliu.vscode-nginx-conf-hint"            # nginx.conf hint
+            "passionkind.prettier-vscode-with-tabs"         # Prettier - JavaScript Formatter
+            "ms-python.python"                  # Python
+            "burkeholland.simple-timer"         # Simple Timer
+            "ms-vscode.sublime-keybindings"     # Sublime Text Keymap and Settings
+            "octref.vetur"                      # Vetur
+        )
+
     for extn in ${RequiredExtensions[@]}; do
         contains InstalledExtensions $extn && present="yes" || present="no"
-        if [[ $present == "yes" ]]; then
-            echo "$extn already installed, skipping..."
-        elif [[ $present == "no" ]]; then
-            echo "installing vscode extension: ${extn}"
+        if test $present = "yes" ; then
+            echo "> $extn already installed, skipping..."
+        else
+            echo "> installing vscode extension: ${extn}"
             code --install-extension ${extn}
         fi
     done
