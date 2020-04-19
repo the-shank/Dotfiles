26595a6742e9f03e621481c4455e1ad722c0e800
Shank <shashanksharma21@gmail.com>
2020-04-19 14:50:19 +0530

    automate OS detection and running pre_install script accordingly

--- pre_install.sh
+++ pre_install.sh
@@ -1,18 +1,98 @@
-# GIT
-if [ ! -f `which git` ]; then
-    echo "Installing git"
-    apt-get update && apt-get install -y git
-else
-    echo "Git already installed"
-fi
+OS='Ubuntu' # possible Manjaro | Ubuntu | OSX
+INSTALL_CMD='sudo apt-get install -y '
+UPDATE_REPO_CMD='sudo apt-get -y update'
+UPDATE_OS_CMD='sudo apt-get -y upgrade'
 
-# ZSH
-if [ ! -f `which zsh` ]; then
-    echo "Installing zsh"
-    apt-get install -y zsh
-else
-    echo "Zsh already installed"
-fi
+function detect_os() {
+    UNAME_A=$(uname -a)
+    if grep -qi "Ubuntu" <<<$UNAME_A; then
+        OS="Ubuntu"
+    elif grep -qi "Manjaro" <<<$UNAME_A; then
+        OS="Manjaro"
+    elif grep -qi "Darwin" <<<$UNAME_A; then
+        OS="OSX"
+    else
+        echo "unknown os: $UNAME_A"
+        exit
+    fi
+
+    echo "detected os: $OS"
+}
+
+function setup_cmds() {
+    case $OS in
+    Ubuntu)
+        INSTALL_CMD='sudo apt-get install -Sy --needed '
+        UPDATE_REPO_CMD='sudo apt-get -y update'
+        UPDATE_OS_CMD='sudo apt-get -y upgrade'
+        ;;
+
+    Manjaro)
+        INSTALL_CMD='sudo pacman -S --needed '
+        UPDATE_REPO_CMD='sudo pacman -Sy'
+        UPDATE_OS_CMD='sudo pacman -Su'
+        ;;
+
+    OSX)
+        INSTALL_CMD='brew install '
+        UPDATE_REPO_CMD='brew update'
+        UPDATE_OS_CMD='brew upgrade'
+        ;;
+
+    *)
+        echo "unsupported OS: $OS"
+        exit
+        ;;
+    esac
+}
+
+function install_utils() {
+    echo "installing utils - start"
+
+    # [alias] -> util
+    declare -A UTILS=(
+        [shfmt]=shfmt
+        [peco]=peco
+        [fd]=fd
+        [rg]=ripgrep
+        [git]=git
+        [zsh]=zsh
+    )
+    echo "required utils: ${UTILS[@]}"
+
+    INSTALLED=()
+    TO_INSTALL=()
+    for util in "${!UTILS[@]}"; do
+        if [ ! -f $(which "$util") ]; then
+            TO_INSTALL+=(${UTILS[$util]})
+        else
+            # echo "$util already installed"
+            INSTALLED+=(${UTILS[$util]})
+        fi
+    done
+    echo "already installed: ${INSTALLED[@]}"
+    echo "to install: ${TO_INSTALL[@]}"
+
+    if [ "${#TO_INSTALL[@]}" -eq "0" ]; then
+        echo "required utils already installed"
+    else
+        $INSTALL_CMD${TO_INSTALL[@]}
+    fi
+
+    echo "installing utils - complete"
+}
+
+function update_os() {
+    $UPDATE_REPO_CMD
+    $UPDATE_OS_CMD
+}
+
+detect_os
+setup_cmds
+update_os
+install_utils
+
+# ZSH ==========================================================================
 # Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
@@ -24,13 +104,19 @@ fi
 # Change default shell
 if [[ ! $SHELL = *zsh ]]; then
     echo 'Changing default shell to zsh'
-    if test "$(uname)" = "Darwin" ; then
-        # osx
-        chsh -s /usr/local/bin/zsh
-    else
-        # linux
+    case $OS in
+    Ubuntu | Manjaro)
         chsh -s /usr/bin/zsh
-    fi
+        ;;
+
+    OSX)
+        chsh -s /usr/local/bin/zsh
+        ;;
+    *)
+        echo "unsupported os: $OS"
+        exit
+        ;;
+    esac
 else
     echo 'Already using zsh'
 fi
@@ -51,47 +137,63 @@ else
     git clone https://github.com/jimeh/zsh-peco-history.git ~/.zsh/zsh-peco-history
 fi
 
-# CREATE DIRECTORIES
+# CREATE DIRECTORIES ===========================================================
 echo "Creating required directories"
 mkdir -p ~/Applications ~/.local/bin ~/Code ~/.ansible/roles ~/Code/UTILS/ANSIBLE-ROLES
 
-# POWERLINE FONTS
+# POWERLINE FONTS ==============================================================
 function install_powerline_fonts() {
     # get the fonts directory
-    if test "$(uname)" = "Darwin" ; then
-        # MacOS
-        font_dir="$HOME/Library/Fonts"
-    else
-        # Linux
+    case $OS in
+    Ubuntu)
         font_dir="$HOME/.local/share/fonts"
-    fi
+        ;;
+
+    Manjaro)
+        font_dir="/usr/share/fonts"
+        ;;
+
+    OSX)
+        font_dir="$HOME/Library/Fonts"
+        ;;
+    *)
+        echo "unsupported os: $OS"
+        exit
+        ;;
+    esac
 
     # check if the powerline fonts have already been installed
-    if [ -e $font_dir/"Ubuntu Mono derivative Powerline.ttf" ]; then
-        installed="true"
+    all_fonts=$(tree $font_dir)
+    if grep -qi powerline <<<$all_fonts; then
+        echo "> powerline fonts already installed"
     else
-        installed="false"
-    fi
-
-    if test installed = "true" ; then
         echo "> installing powerline fonts"
-        if test "$(uname)" = "Darwin" ; then
-            # osx
+        case $OS in
+
+        Ubuntu)
+            $INSTALL_CMD"fonts-powerline"
+            ;;
+
+        Manjaro)
+            yes | $INSTALL_CMD"powerline-fonts"
+            ;;
+
+        OSX)
             git clone https://github.com/powerline/fonts.git --depth=1
             ./fonts/install.sh
             rm -rf fonts
-        else
-            # linux
-            sudo apt-get install -y fonts-powerline
-        fi
-    else
-        echo "> powerline fonts already installed"
+            ;;
+
+        *)
+            echo "unsupported os: $OS"
+            exit
+            ;;
+        esac
     fi
 }
 
-# SPACESHIP PROMPT
-function spaceship_prompt() 
-{
+# SPACESHIP PROMPT =============================================================
+function spaceship_prompt() {
     echo "Setting up spaceship prompt for zsh"
 
     # Powerline Fonts
@@ -101,12 +203,12 @@ function spaceship_prompt()
     ZSH_CUSTOM=$HOME/.oh-my-zsh
     if [ -e "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
         echo "> spaceship theme already downloaded"
-    else 
+    else
         git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
     fi
     if [ -e "$ZSH_CUSTOM/themes/spaceship.zsh-theme" ]; then
         echo "> spaceship theme already linked"
-    else 
+    else
         ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     fi
 
