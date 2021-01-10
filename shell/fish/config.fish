db237b97dea286443227d4bb4086edebb88b5232
Shank <shashanksharma21@gmail.com>
2021-01-10 12:23:11 +0530

    fish | add check to start ssh-agent if not already running

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -12,3 +12,15 @@ source ~/.config/fish/sourced/colored_manpages.fish
 
 # abbreviations
 source ~/.config/fish/sourced/abbreviations.fish
+
+# LOCAL TO THIS MACHINE (MX)
+# somehow the `ssh-agent` that runs be default does not seem to work
+# specifically `ssh-add` is not able to connect to the running `ssh-agent`, even
+# thought `ps aux` tells that `ssh-agent` is running
+# as a temporary workaround, just start another ssh-agent
+if test -z (pgrep ssh-agent)
+    eval (ssh-agent -c) > /dev/null
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
