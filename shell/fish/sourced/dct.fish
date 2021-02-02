902db894c2a35e60a5a957223f60623abf39433b
Shank <shashanksharma21@gmail.com>
2021-02-02 23:15:43 +0530

    fish | fix dct venv activate event listenter

--- shell/fish/sourced/dct.fish
+++ shell/fish/sourced/dct.fish
@@ -8,6 +8,6 @@ end
 
 # virtualfish: event (tp_dl)
 function venv-activated--tp_dl --on-event virtualenv_did_activate:tp_dl
-    cp $HOME/code/dct/tp-data-layer/deploy_data/configs/$PRIMARY_BRAND.config.json \
-        $HOME/code/dct/tp-data-layer/config.json
+    cp $HOME/code/dct/tp-data-layer/deploy_data/configs/tp.config.json \
+       $HOME/code/dct/tp-data-layer/config.json
 end
