c0c7b7f3716e46846e6036a315b12a0b19d7aaf3
Shank <shashanksharma21@gmail.com>
2021-06-13 12:16:48 +0530

    i3 | fix for firefox workspace and multiple script

--- bins/.local/bin/display_multiple.sh
+++ bins/.local/bin/display_multiple.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
-        --output eDP-1-1 --mode 1920x1080 --pos 0x180 --rotate normal
+
+monitor=HDMI-0
+laptop=eDP-1-1
+
+xrandr --output $monitor --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
+        --output $laptop --mode 1920x1080 --pos 0x180 --rotate normal
 
 # restart conky
 pkill conky
 start_conky_maia
 
-# move 7th and 8th workspace to the laptop display
-i3-msg '[workspace="7"] move workspace to output eDP-1-1'
-i3-msg '[workspace="8"] move workspace to output eDP-1-1'
+# move 8th workspace to the laptop display
+i3-msg "[workspace=\"8\"] move workspace to output $laptop"
 
 # move all (except 7th and 8th) workspaces to the main display 
-i3-msg '[workspace="1"] move workspace to output HDMI-0'
-i3-msg '[workspace="2"] move workspace to output HDMI-0'
-i3-msg '[workspace="3"] move workspace to output HDMI-0'
-i3-msg '[workspace="4"] move workspace to output HDMI-0'
-i3-msg '[workspace="5"] move workspace to output HDMI-0'
-i3-msg '[workspace="6"] move workspace to output HDMI-0'
-i3-msg '[workspace="9"] move workspace to output HDMI-0'
-i3-msg '[workspace="10"] move workspace to output HDMI-0'
+i3-msg "[workspace=\"1\"] move workspace to output $monitor"
+i3-msg "[workspace=\"2\"] move workspace to output $monitor"
+i3-msg "[workspace=\"3\"] move workspace to output $monitor"
+i3-msg "[workspace=\"4\"] move workspace to output $monitor"
+i3-msg "[workspace=\"5\"] move workspace to output $monitor"
+i3-msg "[workspace=\"6\"] move workspace to output $monitor"
+i3-msg "[workspace=\"7\"] move workspace to output $monitor"
+i3-msg "[workspace=\"9\"] move workspace to output $monitor"
+i3-msg "[workspace=\"10\"] move workspace to output $monitor"
