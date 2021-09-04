017f70b6e43da8d6e82d74799b3940e2ec5117db
Shank <shashanksharma21@gmail.com>
2021-09-04 09:45:28 -0400

    fish | abbr | change path for miniconda

--- fish/.config/fish/sourced/abbreviations.fish
+++ fish/.config/fish/sourced/abbreviations.fish
@@ -32,7 +32,8 @@ abbr -a ll 'ls -l'
 abbr -a lll 'ls -la'
 abbr -a lr 'ls -R'
 abbr -a less "less -MNi"
-abbr -a conda-enable 'eval /home/shank/Applications/miniconda3/bin/conda "shell.fish" "hook" | source'
+# abbr -a conda-enable 'eval /home/shank/Applications/miniconda3/bin/conda "shell.fish" "hook" | source'
+abbr -a conda-enable 'eval /opt/miniconda3/bin/conda "shell.fish" "hook" | source'
 
 # redshift
 abbr -a r0 'redshift -P -O 6500'
