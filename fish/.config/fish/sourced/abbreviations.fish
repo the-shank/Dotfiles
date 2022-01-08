22de816e2bddd3cb1a3ac375d4c2ea2d02fc05da
Shank <shashanksharma21@gmail.com>
2022-01-07 22:00:28 -0500

    fish | abbr | add abbreviation for `rm -r`

--- fish/.config/fish/sourced/abbreviations.fish
+++ fish/.config/fish/sourced/abbreviations.fish
@@ -34,6 +34,7 @@ abbr -a la 'exa -lah'
 abbr -a lr 'ls -R'
 abbr -a less "less -MNi"
 abbr -a yr 'cal -y'
+abbr -a rmr 'rm -r'
 # abbr -a conda-enable 'eval /home/shank/Applications/miniconda3/bin/conda "shell.fish" "hook" | source'
 abbr -a conda-enable 'eval /opt/miniconda3/bin/conda "shell.fish" "hook" | source'
 
