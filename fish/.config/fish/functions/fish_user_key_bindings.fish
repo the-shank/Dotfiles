85ee6f63b776021b308b59c7a2276670471fb142
Shank <shashanksharma21@gmail.com>
2022-06-03 22:14:24 -0400

    fish | add fzf_key_bindings

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,3 +3,6 @@ function fish_user_key_bindings -d "use Ctrl-f to accept suggestions in vi mode"
         bind -M $mode \cf forward-char
     end
 end
+
+# somehow removing this makes fzf keybindings not being available ¯\_(ツ)_/¯
+fzf_key_bindings
