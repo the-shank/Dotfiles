c99c24caa7d65064067e8e277e478d49394464aa
Shank <shashanksharma21@gmail.com>
2022-06-05 01:12:11 -0400

    cleanup!

--- scripts/tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo 'Installing tmux plugin manager'
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-else
-    echo "tmux plugin manager already installed"
-fi
