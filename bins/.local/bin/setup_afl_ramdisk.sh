696cd3f2b63d725c66d6343f0cc0e29efccca088
Shank <shashanksharma21@gmail.com>
2022-07-20 00:36:52 -0400

    bins | setup_afl_ramdisk: add messages for better feedback

--- bins/.local/bin/setup_afl_ramdisk.sh
+++ bins/.local/bin/setup_afl_ramdisk.sh
@@ -2,15 +2,20 @@
 
 # setup tmpfs on RAM for afl so that it doesnt kill the ssd ¯\_(ツ)_/¯
 
-
 mkdir -p /tmp/afl-ramdisk
+echo "created directory: /tmp/afl-ramdisk"
 chmod 777 /tmp/afl-ramdisk
+echo "changed permission on /tmp/afl-ramdisk to 777"
 
 sudo mount -t tmpfs -o size=512M tmpfs /tmp/afl-ramdisk
+echo "mounted a 512MB tmpfs at /tmp/afl-ramdisk"
 
 mkdir -p /tmp/afl-ramdisk/tmpdir
+echo "created directory: /tmp/afl-ramdisk/tmpdir"
 mkdir -p /tmp/afl-ramdisk/afl_input
+echo "created directory: /tmp/afl-ramdisk/afl_input"
 mkdir -p /tmp/afl-ramdisk/afl_output
+echo "created directory: /tmp/afl-ramdisk/afl_output"
 
 # when running afl-fuzz, point AFL_TMPDIR to this directory
 # an example:
