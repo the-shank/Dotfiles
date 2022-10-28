8859ffa0533ee046e5398dcb80741b5e9a61c90a
Shank <shashanksharma21@gmail.com>
2022-10-28 00:53:04 -0400

    fish | convert aliases to abbr for speeding up the loading time

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -99,7 +99,7 @@ if status --is-login
 end
 
 if type -q bat
-    alias cat="bat --paging=never"
+    abbr -a -g cat "bat --paging=never"
 end
 
 if command -sq fzf && type -q fzf_configure_bindings
@@ -207,276 +207,201 @@ end
 
 ### ALIASES ###
 
-#list
-alias ls="ls --color=auto"
-alias la="ls -a"
-alias ll="ls -alFh"
-alias l="ls"
-alias l.="ls -A | egrep '^\.'"
-
-if type -q exa
-    alias ls="exa"
-    alias xls="exa -a --icons --color=always --group-directories-first"
-    alias xll="exa -lag --icons --color=always --group-directories-first --octal-permissions"
-end
-
-#fix obvious typo's
-alias cd..="cd .."
-alias pdw="pwd"
-alias udpate="sudo pacman -Syyu"
-alias upate="sudo pacman -Syyu"
-alias updte="sudo pacman -Syyu"
-alias updqte="sudo pacman -Syyu"
-alias upqll="paru -Syu --noconfirm"
-alias upal="paru -Syu --noconfirm"
-
 ## Colorize the grep command output for ease of use (good for log files)##
-alias grep="grep --color=auto"
-alias egrep="egrep --color=auto"
-alias fgrep="fgrep --color=auto"
+abbr -a -g grep "grep --color=auto"
+abbr -a -g egrep "egrep --color=auto"
+abbr -a -g fgrep "fgrep --color=auto"
 
 # Color output of ip
-alias ip="ip -color"
+abbr -a -g ip "ip -color"
 
 #readable output
-alias df="df -h"
+abbr -a -g df "df -h"
 
-#keyboard
-alias give-me-azerty-be="sudo localectl set-x11-keymap be"
-alias give-me-qwerty-us="sudo localectl set-x11-keymap us"
+##keyboard
+#alias give-me-azerty-be="sudo localectl set-x11-keymap be"
+#alias give-me-qwerty-us="sudo localectl set-x11-keymap us"
 
 #pacman unlock
-alias unlock="sudo rm /var/lib/pacman/db.lck"
-alias rmpacmanlock="sudo rm /var/lib/pacman/db.lck"
+abbr -a -g unlock "sudo rm /var/lib/pacman/db.lck"
+abbr -a -g rmpacmanlock "sudo rm /var/lib/pacman/db.lck"
 
 #arcolinux logout unlock
-alias rmlogoutlock="sudo rm /tmp/arcologout.lock"
+abbr -a -g rmlogoutlock "sudo rm /tmp/arcologout.lock"
 
 #which graphical card is working
-alias whichvga="/usr/local/bin/arcolinux-which-vga"
+abbr -a -g whichvga "/usr/local/bin/arcolinux-which-vga"
 
 #free
-alias free="free -mt"
+abbr -a -g free "free -mt"
 
 #continue download
-alias wget="wget -c"
+abbr -a -g wget "wget -c"
 
 #userlist
-alias userlist="cut -d: -f1 /etc/passwd"
+abbr -a -g userlist "cut -d: -f1 /etc/passwd"
 
 #merge new settings
-alias merge="xrdb -merge ~/.Xresources"
+abbr -a -g merge "xrdb -merge ~/.Xresources"
 
 # Aliases for software managment
 # pacman
-alias update="sudo pacman -Syyu"
+abbr -a -g update "sudo pacman -Syyu"
 
 # paru as aur helper - updates everything
-alias pksyua="paru -Syu --noconfirm"
-alias upall="paru -Syu --noconfirm"
+abbr -a -g upall "paru -Syu --noconfirm"
 
 #ps
-alias psa="ps auxf"
-alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+abbr -a -g psa "ps auxf"
+abbr -a -g psgrep "ps aux | grep -v grep | grep -i -e VSZ -e"
 
 #grub update
-alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+abbr -a -g update-grub "sudo grub-mkconfig -o /boot/grub/grub.cfg"
 
 #add new fonts
-alias update-fc="sudo fc-cache -fv"
+abbr -a -g update-fc "sudo fc-cache -fv"
 
 #copy/paste all content of /etc/skel over to home folder - backup of config created - beware
 #skel alias has been replaced with a script at /usr/local/bin/skel
 
 #backup contents of /etc/skel to hidden backup folder in home/user
-alias bupskel="cp -Rf /etc/skel ~/.skel-backup-(date +%Y.%m.%d-%H.%M.%S)"
+abbr -a -g bupskel "cp -Rf /etc/skel ~/.skel-backup-(date +%Y.%m.%d-%H.%M.%S)"
 
-#copy shell configs
-alias cb="cp /etc/skel/.bashrc ~/.bashrc && echo "Copied.""
-alias cz="cp /etc/skel/.zshrc ~/.zshrc && echo "Copied.""
-alias cf="cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && exec fish"
+##copy shell configs
+#alias cb="cp /etc/skel/.bashrc ~/.bashrc && echo "Copied.""
+#alias cz="cp /etc/skel/.zshrc ~/.zshrc && echo "Copied.""
+#alias cf="cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && exec fish"
 
-#switch between bash, zsh and fish
-alias tobash="sudo chsh $USER -s /bin/bash && echo 'Done. Now log out.'"
-alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Done. Now log out.'"
-alias tofish="sudo chsh $USER -s /bin/fish && echo 'Done. Now log out.'"
+##switch between bash, zsh and fish
+#alias tobash="sudo chsh $USER -s /bin/bash && echo 'Done. Now log out.'"
+#alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Done. Now log out.'"
+#alias tofish="sudo chsh $USER -s /bin/fish && echo 'Done. Now log out.'"
 
-#switch between lightdm and sddm
-alias tolightdm="sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed ; sudo systemctl enable lightdm.service -f ; echo 'Lightm is active - reboot now'"
-alias tosddm="sudo pacman -S sddm --noconfirm --needed ; sudo systemctl enable sddm.service -f ; echo 'Sddm is active - reboot now'"
-alias toly="sudo pacman -S ly --noconfirm --needed ; sudo systemctl enable ly.service -f ; echo 'Ly is active - reboot now'"
-alias togdm="sudo pacman -S gdm --noconfirm --needed ; sudo systemctl enable gdm.service -f ; echo 'Gdm is active - reboot now'"
-alias tolxdm="sudo pacman -S lxdm --noconfirm --needed ; sudo systemctl enable lxdm.service -f ; echo 'Lxdm is active - reboot now'"
+##switch between lightdm and sddm
+#alias tolightdm="sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed ; sudo systemctl enable lightdm.service -f ; echo 'Lightm is active - reboot now'"
+#alias tosddm="sudo pacman -S sddm --noconfirm --needed ; sudo systemctl enable sddm.service -f ; echo 'Sddm is active - reboot now'"
+#alias toly="sudo pacman -S ly --noconfirm --needed ; sudo systemctl enable ly.service -f ; echo 'Ly is active - reboot now'"
+#alias togdm="sudo pacman -S gdm --noconfirm --needed ; sudo systemctl enable gdm.service -f ; echo 'Gdm is active - reboot now'"
+#alias tolxdm="sudo pacman -S lxdm --noconfirm --needed ; sudo systemctl enable lxdm.service -f ; echo 'Lxdm is active - reboot now'"
 
 # kill commands
-# quickly kill conkies
-alias kc="killall conky"
-# quickly kill polybar
-alias kp="killall polybar"
 # quickly kill picom
-alias kpi="killall picom"
+abbr -a -g kpi "killall picom"
 
 #hardware info --short
-alias hw="hwinfo --short"
-
-#skip integrity check
-alias paruskip="paru -S --mflags --skipinteg"
-alias yayskip="yay -S --mflags --skipinteg"
-alias trizenskip="trizen -S --skipinteg"
+abbr -a -g hw "hwinfo --short"
 
 #check vulnerabilities microcode
-alias microcode="grep . /sys/devices/system/cpu/vulnerabilities/*"
+abbr -a -g microcode "grep . /sys/devices/system/cpu/vulnerabilities/*"
 
-#get fastest mirrors in your neighborhood
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 30 --number 10 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
-#our experimental - best option for the moment
-alias mirrorx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
-alias mirrorxx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 20 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
-alias ram="rate-mirrors --allow-root --disable-comments arch | sudo tee /etc/pacman.d/mirrorlist"
-alias rams="rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist"
+##get fastest mirrors in your neighborhood
+#alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+#alias mirrord="sudo reflector --latest 30 --number 10 --sort delay --save /etc/pacman.d/mirrorlist"
+#alias mirrors="sudo reflector --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
+#alias mirrora="sudo reflector --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
+##our experimental - best option for the moment
+#alias mirrorx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
+#alias mirrorxx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 20 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
+#alias ram="rate-mirrors --allow-root --disable-comments arch | sudo tee /etc/pacman.d/mirrorlist"
+#alias rams="rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist"
 
 #mounting the folder Public for exchange between host and guest on virtualbox
-alias vbm="sudo /usr/local/bin/arcolinux-vbox-share"
-
-#enabling vmware services
-alias start-vmware="sudo systemctl enable --now vmtoolsd.service"
-alias vmware-start="sudo systemctl enable --now vmtoolsd.service"
-alias sv="sudo systemctl enable --now vmtoolsd.service"
-
+abbr -a -g vbm "sudo /usr/local/bin/arcolinux-vbox-share"
 
 #youtube download
-alias yta-aac="yt-dlp --extract-audio --audio-format aac"
-alias yta-best="yt-dlp --extract-audio --audio-format best"
-alias yta-flac="yt-dlp --extract-audio --audio-format flac"
-alias yta-mp3="yt-dlp --extract-audio --audio-format mp3"
-alias ytv-best="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
+abbr -a -g yta-aac "yt-dlp --extract-audio --audio-format aac"
+abbr -a -g yta-best "yt-dlp --extract-audio --audio-format best"
+abbr -a -g yta-flac "yt-dlp --extract-audio --audio-format flac"
+abbr -a -g yta-mp3 "yt-dlp --extract-audio --audio-format mp3"
+abbr -a -g ytv-best "yt-dlp -f 'bestvideo[ext mp4]+bestaudio[ext m4a]/bestvideo+bestaudio' --merge-output-format mp4"
 
 #Recent Installed Packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+abbr -a -g rip "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+abbr -a -g riplong "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
 
 #iso and version used to install ArcoLinux
-alias iso="cat /etc/dev-rel | awk -F '=' '/ISO/ {print $2}'"
+abbr -a -g iso "cat /etc/dev-rel | awk -F '=' '/ISO/ {print $2}'"
 
 #Cleanup orphaned packages
-alias cleanup="sudo pacman -Rns (pacman -Qtdq)"
+abbr -a -g cleanup "sudo pacman -Rns (pacman -Qtdq)"
 
 #clear
 alias clean="clear; seq 1 (tput cols) | sort -R | sparklines | lolcat"
 
-#search content with ripgrep
-alias rg="rg --sort path"
+##search content with ripgrep
+#abbr -a -g rg "rg --sort path"
 
 #get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
+abbr -a -g jctl "journalctl -p 3 -xb"
 
-#nano for important configuration files
-#know what you do in these files
-alias nlxdm="sudo $EDITOR /etc/lxdm/lxdm.conf"
-alias nlightdm="sudo $EDITOR /etc/lightdm/lightdm.conf"
-alias npacman="sudo $EDITOR /etc/pacman.conf"
-alias ngrub="sudo $EDITOR /etc/default/grub"
-alias nconfgrub="sudo $EDITOR /boot/grub/grub.cfg"
-alias nmkinitcpio="sudo $EDITOR /etc/mkinitcpio.conf"
-alias nmirrorlist="sudo $EDITOR /etc/pacman.d/mirrorlist"
-alias narcomirrorlist="sudo $EDITOR /etc/pacman.d/arcolinux-mirrorlist"
-alias nsddm="sudo $EDITOR /etc/sddm.conf"
-alias nsddmk="sudo $EDITOR /etc/sddm.conf.d/kde_settings.conf"
-alias nfstab="sudo $EDITOR /etc/fstab"
-alias nnsswitch="sudo $EDITOR /etc/nsswitch.conf"
-alias nsamba="sudo $EDITOR /etc/samba/smb.conf"
-alias ngnupgconf="sudo $EDITOR /etc/pacman.d/gnupg/gpg.conf"
-alias nhosts="sudo $EDITOR /etc/hosts"
-alias nhostname="sudo $EDITOR /etc/hostname"
-alias nb="$EDITOR ~/.bashrc"
-alias nz="$EDITOR ~/.zshrc"
-alias nf="$EDITOR ~/.config/fish/config.fish"
+# nvime for important configuration files
+abbr -a -g nf "$EDITOR ~/.config/fish/config.fish"
+abbr -a -g nt "$EDITOR ~/.tmux.conf"
 
-#reading logs with bat
-alias lcalamares="bat /var/log/Calamares.log"
-alias lpacman="bat /var/log/pacman.log"
-alias lxorg="bat /var/log/Xorg.0.log"
-alias lxorgo="bat /var/log/Xorg.0.log.old"
+##reading logs with bat
+#alias lcalamares="bat /var/log/Calamares.log"
+#alias lpacman="bat /var/log/pacman.log"
+#alias lxorg="bat /var/log/Xorg.0.log"
+#alias lxorgo="bat /var/log/Xorg.0.log.old"
 
 #gpg
 #verify signature for isos
-alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
-alias fix-gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+abbr -a -g gpg-check "gpg2 --keyserver-options auto-key-retrieve --verify"
+abbr -a -g fix-gpg-check "gpg2 --keyserver-options auto-key-retrieve --verify"
 #receive the key of a developer
-alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
-alias fix-gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
-alias fix-keyserver="[ -d ~/.gnupg ] || mkdir ~/.gnupg ; cp /etc/pacman.d/gnupg/gpg.conf ~/.gnupg/ ; echo 'done'"
+abbr -a -g gpg-retrieve "gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+abbr -a -g fix-gpg-retrieve "gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+abbr -a -g fix-keyserver "[ -d ~/.gnupg ] || mkdir ~/.gnupg ; cp /etc/pacman.d/gnupg/gpg.conf ~/.gnupg/ ; echo 'done'"
 
 #fixes
-alias fix-permissions="sudo chown -R $USER:$USER ~/.config ~/.local"
-alias keyfix="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias key-fix="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias keys-fix="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias fixkey="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias fixkeys="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias fix-key="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias fix-keys="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
-alias fix-sddm-config="/usr/local/bin/arcolinux-fix-sddm-config"
-alias fix-pacman-conf="/usr/local/bin/arcolinux-fix-pacman-conf"
-alias fix-pacman-keyserver="/usr/local/bin/arcolinux-fix-pacman-gpg-conf"
+abbr -a -g fix-permissions "sudo chown -R $USER:$USER ~/.config ~/.local"
+abbr -a -g keyfix "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g key-fix "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g keys-fix "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g fixkey "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g fixkeys "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g fix-key "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g fix-keys "/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
+abbr -a -g fix-sddm-config "/usr/local/bin/arcolinux-fix-sddm-config"
+abbr -a -g fix-pacman-conf "/usr/local/bin/arcolinux-fix-pacman-conf"
+abbr -a -g fix-pacman-keyserver "/usr/local/bin/arcolinux-fix-pacman-gpg-conf"
 
 #maintenance
-alias big="expac -H M "%m\t%n" | sort -h | nl"
-alias downgrada="sudo downgrade --ala-url https://ant.seedhost.eu/arcolinux/"
+abbr -a -g big "expac -H M "%m\t%n" | sort -h | nl"
+abbr -a -g downgrada "sudo downgrade --ala-url https://ant.seedhost.eu/arcolinux/"
 
 #hblock (stop tracking with hblock)
 #use unhblock to stop using hblock
-alias unhblock="hblock -S none -D none"
+abbr -a -g unhblock "hblock -S none -D none"
 
 #systeminfo
-alias probe="sudo -E hw-probe -all -upload"
-alias sysfailed="systemctl list-units --failed"
+abbr -a -g probe "sudo -E hw-probe -all -upload"
+abbr -a -g sysfailed "systemctl list-units --failed"
 
 #shutdown or reboot
-alias ssn="sudo shutdown now"
-alias sr="sudo reboot"
+abbr -a -g ssn "sudo shutdown now"
+abbr -a -g sr "sudo reboot"
 
 #update betterlockscreen images
-alias bls="betterlockscreen -u /usr/share/backgrounds/arcolinux/"
+abbr -a -g bls "betterlockscreen -u /usr/share/backgrounds/arcolinux/"
 
 #give the list of all installed desktops - xsessions desktops
-alias xd="ls /usr/share/xsessions"
-
-#btrfs aliases
-alias btrfsfs="sudo btrfs filesystem df /"
-alias btrfsli="sudo btrfs su li / -t"
-
-#snapper aliases
-alias snapcroot="sudo snapper -c root create-config /"
-alias snapchome="sudo snapper -c home create-config /home"
-alias snapli="sudo snapper list"
-alias snapcr="sudo snapper -c root create"
-alias snapch="sudo snapper -c home create"
-
-#Leftwm aliases
-alias lti="leftwm-theme install"
-alias ltu="leftwm-theme uninstall"
-alias lta="leftwm-theme apply"
-alias ltupd="leftwm-theme update"
-alias ltupg="leftwm-theme upgrade"
+abbr -a -g xd "ls /usr/share/xsessions"
 
 #arcolinux applications
-alias att="archlinux-tweak-tool"
-alias adt="arcolinux-desktop-trasher"
-alias abl="arcolinux-betterlockscreen"
-alias agm="arcolinux-get-mirrors"
-alias amr="arcolinux-mirrorlist-rank-info"
-alias aom="arcolinux-osbeck-as-mirror"
-alias ars="arcolinux-reflector-simple"
-alias atm="arcolinux-tellme"
-alias avs="arcolinux-vbox-share"
-alias awa="arcolinux-welcome-app"
+abbr -a -g att "archlinux-tweak-tool"
+abbr -a -g adt "arcolinux-desktop-trasher"
+abbr -a -g abl "arcolinux-betterlockscreen"
+abbr -a -g agm "arcolinux-get-mirrors"
+abbr -a -g amr "arcolinux-mirrorlist-rank-info"
+abbr -a -g aom "arcolinux-osbeck-as-mirror"
+abbr -a -g ars "arcolinux-reflector-simple"
+abbr -a -g atm "arcolinux-tellme"
+abbr -a -g avs "arcolinux-vbox-share"
+abbr -a -g awa "arcolinux-welcome-app"
 
 #remove
-alias rmgitcache="rm -r ~/.cache/git"
+abbr -a -g rmgitcache "rm -r ~/.cache/git"
 
 #moving your personal files and folders from /personal to ~
 function personal
@@ -487,7 +412,7 @@ end
 # git
 # using plugin
 # omf install https://github.com/jhillyerd/plugin-git
-alias undopush "git push -f origin HEAD^:master"
+abbr -a -g undopush "git push -f origin HEAD^:master"
 
 # reporting tools - install when not installed
 # neofetch
@@ -542,10 +467,10 @@ set fish_color_search_match --background="#60AEFF"
 source ~/.config/fish/sourced/abbreviations.fish
 
 # map caps to escape
-setxkbmap -option caps:escape
+# setxkbmap -option caps:escape
 
 # set keyboard repeat rate
-xset r rate 300 45 
+# xset r rate 300 45 
 
 # ssh-agent
 # source: https://unix.stackexchange.com/a/132117
