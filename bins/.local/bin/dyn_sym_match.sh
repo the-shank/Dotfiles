c8cba4dc22585f3c51f1e0c04d34e79c55ff8823
Shank <shashanksharma21@gmail.com>
2022-11-03 23:23:18 -0400

    bins | dyn_sym_match | script to see which undefined symbols can be resolved by the shared library

---
+++ bins/.local/bin/dyn_sym_match.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this script find the number of undefined symbols for the first argument
+# that can be resolved by the second argument
+
+# check that we have two arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <file> <library>"
+    exit 1
+fi
+
+# check that both arguments are files
+if [ ! -f $1 ]; then
+    echo "$1 is not a file"
+    exit 1
+fi
+if [ ! -f $2 ]; then
+    echo "$2 is not a file"
+    exit 1
+fi
+
+# get the list of undefined symbols for the first argument
+undefined_symbols_fst=$(llvm-readelf --dyn-syms --demangle $1 | rg --ignore-case " und " | awk 'BEGIN {FS=" UND "};{print $2}' | sed -e 's/@.*//g' | sort)
+
+# get he list of symbols defined in the second argument
+defined_symbols_snd=$(nm -D --demangle $2 | rg --ignore-case " T " | awk 'BEGIN {FS=" T "};{print $2}' | sort)
+
+comm -12 <(echo "$undefined_symbols_fst") <(echo "$defined_symbols_snd")
