4abd87280b14e3e83543d045ae3637e38a89b2da
Shank <shashanksharma21@gmail.com>
2022-12-15 03:50:28 -0500

    nvim | copilot and treesitter setup

--- neovim/.config/nvim/lua/user/treesitter.lua
+++ neovim/.config/nvim/lua/user/treesitter.lua
@@ -9,11 +9,11 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
-  -- ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = "all", -- one of "all" or a list of languages
+  -- ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
+
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
