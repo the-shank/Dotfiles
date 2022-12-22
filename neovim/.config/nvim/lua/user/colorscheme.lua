14d4aeb767ccfe246c31a83bddf7d0d73475a276
Shank <shashanksharma21@gmail.com>
2022-12-21 23:28:17 -0500

    nvim | colorscheme | add option for light mode too

--- neovim/.config/nvim/lua/user/colorscheme.lua
+++ neovim/.config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
 -- local colorscheme = "tokyonight-night"
-vim.g.everforest_background = 'hard'
+-- vim.opt.background = "light"
+vim.opt.background = "dark"
+vim.g.everforest_background = "hard"
 local colorscheme = "everforest"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
