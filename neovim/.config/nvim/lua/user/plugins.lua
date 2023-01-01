04cce156d3dd8588d9998549325f1d50a4beed3d
Shank <shashanksharma21@gmail.com>
2023-01-01 16:35:59 -0500

    nvim | add treesitter-textobjects, enabling to peek definitions

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -92,10 +92,7 @@ return packer.startup(function(use)
 	-- Treesitter
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
     use "nvim-treesitter/nvim-treesitter-context"
-	-- use {
-	--   "nvim-treesitter/nvim-treesitter",
-	--   commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
-	-- }
+    use "nvim-treesitter/nvim-treesitter-textobjects"
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" })
