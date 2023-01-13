be8cd7e39a3b3e6f2517f928e7b1011150bb313d
Shank <shashanksharma21@gmail.com>
2023-01-13 10:32:31 -0500

    nvim | switch to different plugin for lsp progress status in lualine

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,15 +2,15 @@ local wezterm = require("wezterm")
 
 return {
 	font = wezterm.font("Iosevka Nerd Font", {
-        weight = "Regular"
-        -- weight = "Medium"
-    }),
-    font_size = 12,
-    color_scheme = "nordfox",
-    window_frame = {
-        font_size = 11,
-    },
-    hide_tab_bar_if_only_one_tab = true,
+		weight = "Regular",
+		-- weight = "Medium"
+	}),
+	font_size = 12,
+	color_scheme = "nordfox",
+	window_frame = {
+		font_size = 11,
+	},
+	hide_tab_bar_if_only_one_tab = true,
 	-- tab_bar_at_bottom = true,
 	window_padding = {
 		left = 2,
