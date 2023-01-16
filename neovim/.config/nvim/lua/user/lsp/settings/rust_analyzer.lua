c8d2413b331b7d7adedcb2c896b26ef6e948dd0c
Shank <shashanksharma21@gmail.com>
2023-01-16 17:47:47 -0500

    nvim | still struggling with rust-analyzer config

--- neovim/.config/nvim/lua/user/lsp/settings/rust_analyzer.lua
+++ neovim/.config/nvim/lua/user/lsp/settings/rust_analyzer.lua
@@ -1,15 +1,30 @@
 return {
-    settings = {
-        -- ["rust-analyzer"] = {
-        rust_analyzer = {
-            rustfmt = {
-                rangeFormatting = {
-                    enable = true,
-                },
-            },
-            cargo = {
-                allFeatures = true,
-            }
-        },
-    },
+	settings = {
+		["rust-analyzer"] = {
+			rustfmt = {
+				rangeFormatting = {
+					enable = true,
+				},
+			},
+			cargo = {
+				allFeatures = true,
+			},
+			check_on_save = {
+				command = "clippy",
+				extraArgs = {
+					"--no-deps",
+					"--",
+					"--warn",
+					"clippy::pedantic",
+				},
+			},
+			workspace = {
+				symbol = {
+					search = {
+						kind = "all_symbols",
+					},
+				},
+			},
+		},
+	},
 }
