0bdef18325a215eb64486e517a7ab4e843ab4ffe
Shank <shashanksharma21@gmail.com>
2023-01-16 18:56:04 -0500

    nvim | sort buffers in last used order

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -64,7 +64,7 @@ keymap(
 )
 keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers sort_lastused=true<CR>", opts)
 keymap("n", "<leader>fo", ":Telescope lsp_document_symbols<CR>", opts)
 keymap("n", "<leader>fO", ":Telescope lsp_workspace_symbols<CR>", opts)
 keymap("n", "<leader>c", ":Telescope commands<CR>", opts)
