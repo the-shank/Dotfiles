a0316fd77a2df059e065e2bbee7a340f5c9d0325
Shank <shashanksharma21@gmail.com>
2023-01-20 01:27:58 -0500

    nvim | switch to tokyonight-storm theme

--- neovim/.config/nvim/lua/user/colorscheme.lua
+++ neovim/.config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,12 @@
--- local colorscheme = "tokyonight-night"
 -- vim.opt.background = "light"
 vim.opt.background = "dark"
-vim.g.everforest_background = "hard"
-local colorscheme = "everforest"
+
+-- vim.g.everforest_background = "hard"
+-- local colorscheme = "everforest"
+
+local colorscheme = "tokyonight-storm"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
