574a9e74c9ce2e95ff1f24bb180951abd2fd8176
Shank <shashanksharma21@gmail.com>
2023-01-20 15:47:23 -0500

    nvim | dont use italics in comments

--- neovim/.config/nvim/lua/user/colorscheme.lua
+++ neovim/.config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,11 @@
 -- vim.opt.background = "light"
 vim.opt.background = "dark"
 
--- vim.g.everforest_background = "hard"
--- local colorscheme = "everforest"
+vim.g.everforest_background = "hard"
+vim.g.everforest_disable_italic_comment = 1
+local colorscheme = "everforest"
 
-local colorscheme = "tokyonight-storm"
+-- local colorscheme = "tokyonight-storm"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
