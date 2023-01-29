b7f9951c6924f0d2f579718fcdc5edf1d04c168c
Shank <shashanksharma21@gmail.com>
2023-01-28 19:27:24 -0500

    nvim | keybinding to send telescope search results to trouble

--- neovim/.config/nvim/lua/user/telescope.lua
+++ neovim/.config/nvim/lua/user/telescope.lua
@@ -1,26 +1,33 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-local actions = require "telescope.actions"
+local status_ok, trouble = pcall(require, "trouble.providers.telescope")
+if not status_ok then
+	print("failed: loading trouble")
+	return
+end
 
-telescope.setup {
-  defaults = {
+local actions = require("telescope.actions")
 
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules" },
+telescope.setup({
+	defaults = {
 
-    mappings = {
-      i = {
-        ["<Down>"] = actions.cycle_history_next,
-        ["<Up>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<A-d>"] = actions.delete_buffer,
-      },
-    },
-  },
-}
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		file_ignore_patterns = { ".git/", "node_modules" },
+
+		mappings = {
+			i = {
+				["<Down>"] = actions.cycle_history_next,
+				["<Up>"] = actions.cycle_history_prev,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<A-d>"] = actions.delete_buffer,
+				["<C-t>"] = trouble.open_with_trouble,
+			},
+		},
+	},
+})
