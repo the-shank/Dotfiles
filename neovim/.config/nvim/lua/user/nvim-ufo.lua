148db278060f642e3f3a5368d0be62f3cb7ec923
Shank <shashanksharma21@gmail.com>
2023-02-03 13:59:37 -0500

    nvim | use better characters for folds

--- neovim/.config/nvim/lua/user/nvim-ufo.lua
+++ neovim/.config/nvim/lua/user/nvim-ufo.lua
@@ -8,6 +8,7 @@ vim.opt.foldcolumn = "1" -- '0' is not bad
 vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
+vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
