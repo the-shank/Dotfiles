dd644843768e16db11e1498ec767f80107fb93ad
Shank <shashanksharma21@gmail.com>
2023-02-03 19:46:59 -0500

    nvim | fix nvim-ufo to work with nvim-lsp

--- neovim/.config/nvim/lua/user/nvim-ufo.lua
+++ neovim/.config/nvim/lua/user/nvim-ufo.lua
@@ -14,30 +14,29 @@ vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
--- Option 2: nvim lsp as LSP client
--- Tell the server the capability of foldingRange,
--- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-	require("lspconfig")[ls].setup({
-		capabilities = capabilities,
-		-- you can add other fields for setting up lsp server in this table
-	})
-end
+-- -- Option 2: nvim lsp as LSP client
+-- -- Tell the server the capability of foldingRange,
+-- -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.foldingRange = {
+-- 	dynamicRegistration = false,
+-- 	lineFoldingOnly = true,
+-- }
+-- local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+-- for _, ls in ipairs(language_servers) do
+-- 	require("lspconfig")[ls].setup({
+-- 		capabilities = capabilities,
+-- 		-- you can add other fields for setting up lsp server in this table
+-- 	})
+-- end
 nvim_ufo.setup()
 
--- -- Option 3: treesitter as a main provider instead
--- -- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
--- -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+-- Option 3: treesitter as a main provider instead
+-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
 -- use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
--- require("ufo").setup({
+-- nvim_ufo.setup({
 -- 	provider_selector = function(bufnr, filetype, buftype)
 -- 		return { "treesitter", "indent" }
 -- 	end,
 -- })
--- --
