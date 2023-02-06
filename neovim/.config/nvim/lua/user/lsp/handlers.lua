f5826cb9fea2ad22c6ff6bfcf5c12cb219fe5444
Shank <shashanksharma21@gmail.com>
2023-02-06 18:21:16 -0500

    nvim | add keybinding for going to type-definition

--- neovim/.config/nvim/lua/user/lsp/handlers.lua
+++ neovim/.config/nvim/lua/user/lsp/handlers.lua
@@ -59,8 +59,9 @@ end
 local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	local keymap = vim.api.nvim_buf_set_keymap
-	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	-- keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	-- keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)  -> trouble.nvim (see keymaps.lua)
