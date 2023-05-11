0537443a9c7691dcd562230ec5bf21b1f108d18a
Shank <shashanksharma21@gmail.com>
2023-05-11 14:35:59 -0400

    nvim | telescope | use default path in picker display

--- neovim/.config/nvim/lua/user/telescope.lua
+++ neovim/.config/nvim/lua/user/telescope.lua
@@ -16,7 +16,7 @@ telescope.setup({
 
 		prompt_prefix = " ",
 		selection_caret = " ",
-		path_display = { "smart" },
+		-- path_display = { "smart" },
 		file_ignore_patterns = { ".git/", "node_modules" },
 
 		mappings = {
