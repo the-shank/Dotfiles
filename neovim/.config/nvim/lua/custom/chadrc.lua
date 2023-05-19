8e5078137e167bc814cd04b2aa7467944eff52c1
Shank <shashanksharma21@gmail.com>
2023-05-19 09:59:52 -0400

    nvim | set light theme to everforest_light

--- neovim/.config/nvim/lua/custom/chadrc.lua
+++ neovim/.config/nvim/lua/custom/chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "everforest",
-  theme_toggle = { "everforest", "one_light" },
+  theme_toggle = { "everforest", "everforest_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
