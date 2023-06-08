a666bed69aff419399e2873603cd197e23ced33b
Shank <shashanksharma21@gmail.com>
2023-06-08 03:30:17 -0400

    nvim | navigate help text

--- neovim/.config/nvim/lua/custom/init.lua
+++ neovim/.config/nvim/lua/custom/init.lua
@@ -1,4 +1,5 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
@@ -6,5 +7,13 @@
 --   command = "tabdo wincmd =",
 -- })
 
-local opt = vim.opt
+-- set rulers
 opt.colorcolumn = { 100, 120 }
+
+-- navigate help text using <CR> to go to item under cursor
+autocmd({ "FileType" }, {
+  pattern = { "help" },
+  callback = function()
+    vim.cmd [[ nnoremap <buffer> <CR> <C-]> ]]
+  end,
+})
