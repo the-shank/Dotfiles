ca6b4d348e35cd8562ac8732e1ef5b8ccbbf3dd6
Shank <shashanksharma21@gmail.com>
2023-06-08 01:31:24 -0400

    nvim | stay in indent mode and center search results

--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -2,41 +2,55 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-Up>"] = { "<cmd> resize -1 <CR>", "increase size up", opts = { nowait = true } },
-    ["<C-Down>"] = { "<cmd> resize +1 <CR>", "increase size down", opts = { nowait = true } },
-    ["<C-Left>"] = { "<cmd> vertical resize -1 <CR>", "increase size left", opts = { nowait = true } },
-    ["<C-Right>"] = { "<cmd> vertical resize +1 <CR>", "increase size right", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<C-Up>"] = { "<cmd> resize -1 <CR>", "increase size up", opts = { nowait = true } },
+		["<C-Down>"] = { "<cmd> resize +1 <CR>", "increase size down", opts = { nowait = true } },
+		["<C-Left>"] = { "<cmd> vertical resize -1 <CR>", "increase size left", opts = { nowait = true } },
+		["<C-Right>"] = { "<cmd> vertical resize +1 <CR>", "increase size right", opts = { nowait = true } },
+
+		-- center search results
+		["n"] = { "nzz", "next", opts = { nowait = true } },
+		["N"] = { "Nzz", "previous", opts = { nowait = true } },
+		["*"] = { "*zz", "next", opts = { nowait = true } },
+		["#"] = { "#zz", "next", opts = { nowait = true } },
+	},
+
+	v = {
+		["<"] = { "<gv", "indent left", opts = { nowait = true } },
+		[">"] = { ">gv", "indent left", opts = { nowait = true } },
+	},
 }
 
 -- more keybinds!
 
 M.telescope = {
-  n = {
-    ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
-    -- ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true ignore-case exclude=target/ exclude=.git/ exclude=.cache/ <CR>", "Find all" },
-    ["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Document Symbols"},
-    ["<leader>fS"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Workspace Symbols"},
-  },
+	n = {
+		["<leader>ff"] = {
+			"<cmd> Telescope find_files find_command=fd,--hidden,--ignore-case,--follow,--strip-cwd-prefix,--no-ignore-vcs,--exclude,target/,--exclude,.git/,--exclude,.cache/ <CR>",
+		},
+		["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
+		-- ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true ignore-case exclude=target/ exclude=.git/ exclude=.cache/ <CR>", "Find all" },
+		["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Document Symbols" },
+		["<leader>fS"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Workspace Symbols" },
+	},
 }
 
 M.symbols_outline = {
-  n = {
-    ["<leader>o"] = { "<cmd> SymbolsOutline <CR>" },
-  },
+	n = {
+		["<leader>o"] = { "<cmd> SymbolsOutline <CR>" },
+	},
 }
 
 M.lspconfig = {
-  n = {
-    ["gD"] = {
-      function()
-        vim.lsp.buf.type_definition()
-      end,
-      "LSP type definition",
-    },
-  }
+	n = {
+		["gD"] = {
+			function()
+				vim.lsp.buf.type_definition()
+			end,
+			"LSP type definition",
+		},
+	},
 }
 
 return M
