aaada848739e71ac3e9567cbffab3015d990d36b
Shank <shashanksharma21@gmail.com>
2023-06-17 09:22:35 -0400

    bins | bash_helps | add some tests

--- bins/.local/bash_helpers/utils.sh
+++ bins/.local/bash_helpers/utils.sh
@@ -4,28 +4,114 @@
 # Intended usage of this script is to include it in other bash scripts.
 #
 # Current list of util functions:
-# 01. ensure_one_arg
-# 02. ensure_one_arg_exact
+# 01. ensure_equal <arg1> <arg2>
+# 02. ensure_one_arg <num>
+# 03. ensure_one_arg_exact <num>
 # =======================================================================================
 
 
-# 01. ensure_one_arg
-# Args: None
-# Returns: None
-ensure_one_arg(){
-  if [[ $# < 1 ]]; then
-    echo "[!] less than 1 arg provided"
+# Args:
+#   - <num1>
+#   - "lt" | "le" | "eq" | "gt" | "ge"
+#   - <num2>
+#   - [<error_msg>]
+ensure_cmp(){
+  if [[ $# < 3 ]]; then
+    echo "[!] ensure_cmp expects atleast 3 args"
+    echo "[!] Usage: enquare_cmp <num1> \"lt\" <num2> [<error_msg>]"
     exit 1
   fi
+
+  n1=$1
+  op=$2
+  n2=$3
+  local error_msg=""
+  if [[ $# == 3 ]]; then
+    error_msg="[!] $3"
+  fi
+
+  case $op in
+    "lt")
+      if ! [[ $n1 < $n2 ]]; then
+        echo "$error_msg"
+        exit 1
+      fi
+      ;;
+
+    "le")
+      if ! [[ $n1 -le $n2 ]]; then
+        echo "$error_msg"
+        exit 1
+      fi
+      ;;
+
+    "gt")
+      if ! [[ $n1 > $n2 ]]; then
+        echo "$error_msg"
+        exit 1
+      fi
+      ;;
+
+    "ge")
+      if ! [[ $n1 -ge $n2 ]]; then
+        echo "$error_msg"
+        exit 1
+      fi
+      ;;
+
+    "eq")
+      if ! [[ $n1 == $n2 ]]; then
+        echo "$error_msg"
+        exit 1
+      fi
+      ;;
+
+    *)
+      echo "[!] invalid op: $op"
+      exit 1
+      ;;
+      
+  esac
+}
+
+# 01. ensure_equal
+# Args:
+#   - <num1>
+#   - <num2>
+#   - <error_msg>
+# Returns: None
+ensure_equal() {
+  # atleast 2 args required
+  ensure_cmp $# "ge" 2 "ensure_equal expects 2 args (nums to be compared), and optionally an error msg"
+
+  local fst=$1
+  local snd=$2
+  local error_msg=""
+  if [[ $# == 3 ]]; then
+    error_msg="[!] $3"
+  fi
+  ensure_cmp $fst "eq" $snd $error_msg
+}
+
+# 02. ensure_one_arg
+# Args: 
+#   - <num_args>
+# Returns: None
+ensure_one_arg(){
+  ensure_equal $# 1 "ensure_one_arg expects one arg, which is a number"
+
+  local num_args=$1
+  ensure_cmp $num_args "ge" 1 "expected 1 arg"
 }
 
 
-# 02. ensure_one_arg_exact
-# Args: None
+# 03. ensure_one_arg_exact
+# Args: 
+#   - <num_args>
 # Returns: None
 ensure_one_arg_exact(){
-  if [[ $# != 1 ]]; then
-    echo "[!] exactly 1 arg should be provided however $# args were provided"
-    exit 1
-  fi
+  ensure_equal $# 1 "ensure_one_arg_exact expects one arg, which is a number"
+
+  local num_args=$1
+  ensure_equal $num_args 1 "expected exactly 1 arg"
 }
---
+++ bins/.local/bash_helpers/utils_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curr_dir="$(realpath $(dirname "${BASH_SOURCE[0]}"))" 
+
+SRC_FILE="utils.sh"
+
+source "$curr_dir/$SRC_FILE"
+
+ensure_cmp 1 "eq" 1 "Failed: ensure_cmp 1 \"eq\" 1"
+ensure_cmp 1 "lt" 2 "Failed: ensure_cmp 1 \"lt\" 2"
+ensure_cmp 1 "le" 1 "Failed: ensure_cmp 1 \"le\" 1"
+ensure_cmp 2 "gt" 1 "Failed: ensure_cmp 2 \"gt\" 1"
+ensure_cmp 1 "ge" 1 "Failed: ensure_cmp 1 \"ge\" 1"
+
+ensure_equal 1 1 "Failed: enqure_equal 1 1"
+
+echo "[*] All tests passed"
