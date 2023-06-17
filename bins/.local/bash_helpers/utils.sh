486ae60630c25520258bee45101d930d29df3426
Shank <shashanksharma21@gmail.com>
2023-06-17 08:31:36 -0400

    bins | add bash_helpers (ensure_one_arg and ensure_one_arg_exact)

---
+++ bins/.local/bash_helpers/utils.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# =======================================================================================
+# Intended usage of this script is to include it in other bash scripts.
+#
+# Current list of util functions:
+# 01. ensure_one_arg
+# 02. ensure_one_arg_exact
+# =======================================================================================
+
+
+# 01. ensure_one_arg
+# Args: None
+# Returns: None
+ensure_one_arg(){
+  if [[ $# < 1 ]]; then
+    echo "[!] less than 1 arg provided"
+    exit 1
+  fi
+}
+
+
+# 02. ensure_one_arg_exact
+# Args: None
+# Returns: None
+ensure_one_arg_exact(){
+  if [[ $# != 1 ]]; then
+    echo "[!] exactly 1 arg should be provided"
+    exit 1
+  fi
+}
