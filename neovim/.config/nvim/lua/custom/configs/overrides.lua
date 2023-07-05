ff5fb3428ce521c542f5948c6f118e60aafcb497
Shank <shashanksharma21@gmail.com>
2023-07-05 00:28:24 -0400

    nvim | dont need deno lsp

--- neovim/.config/nvim/lua/custom/configs/overrides.lua
+++ neovim/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,77 +1,76 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "bash",
-    "c",
-    "cmake",
-    "cpp",
-    "css",
-    "dockerfile",
-    "html",
-    "javascript",
-    "llvm",
-    "lua",
-    "make",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "rust",
-    "solidity",
-    "toml",
-    "tsx",
-    "typescript",
-    "vim",
-    "yaml",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"cpp",
+		"css",
+		"dockerfile",
+		"html",
+		"javascript",
+		"llvm",
+		"lua",
+		"make",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"rust",
+		"solidity",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"prettier",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- rust
-    "rust-analyzer",
+		-- rust
+		"rust-analyzer",
 
-    -- python
-    "pyright",
-  },
+		-- python
+		"pyright",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-    ignore = false,
-  },
+	git = {
+		enable = true,
+		ignore = false,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 return M
