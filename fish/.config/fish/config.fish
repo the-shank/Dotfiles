4e82df81b40449eac7c6d05bbccc604e39659b19
Shank <shashanksharma21@gmail.com>
2023-07-20 13:04:39 -0400

    fish | colored less

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -83,8 +83,8 @@ set fish_greeting
 # colored manpages
 # https://blog.yossarian.net/2018/01/22/Reading-Manpages-Like-a-Pro
 # https://github.com/jonhoo/configs/blob/master/shell/.config/fish/config.fish
-setenv LESS_TERMCAP_mb \e'[1;31m'
-setenv LESS_TERMCAP_md \e'[1;31m'
+setenv LESS_TERMCAP_mb \e'[01;31m'
+setenv LESS_TERMCAP_md \e'[01;31m'
 setenv LESS_TERMCAP_me \e'[0m'
 setenv LESS_TERMCAP_se \e'[0m'
 setenv LESS_TERMCAP_so \e'[1;44;33m'
