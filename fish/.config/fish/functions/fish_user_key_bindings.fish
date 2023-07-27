d2b4392524da363c00bd24e9d0006e7ab0ca1167
Shank <shashanksharma21@gmail.com>
2023-07-27 01:59:22 -0400

    fish | bind alt-f to accept one word from suggestion

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,7 @@ function fish_user_key_bindings -d "use Ctrl-f to accept suggestions in vi mode"
     fish_vi_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
+        bind -M $mode \ef nextd-or-forward-word
     end
 end
 
