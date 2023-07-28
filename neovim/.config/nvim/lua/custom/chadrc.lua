44c1ffa54ff5ef2f01fcd21f1880e6a4b541cc9e
Shank <shashanksharma21@gmail.com>
2023-07-28 04:34:11 -0400

    nvim | chadrc | update statusline as per new version of nvchad

--- neovim/.config/nvim/lua/custom/chadrc.lua
+++ neovim/.config/nvim/lua/custom/chadrc.lua
@@ -2,55 +2,56 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "everforest_light",
-  theme_toggle = { "everforest_light", "everforest_light_light" },
+	theme = "everforest_light",
+	theme_toggle = { "everforest_light", "everforest_light_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
 
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "███████ ██   ██  █████  ███    ██ ██   ██",
-      "██      ██   ██ ██   ██ ████   ██ ██  ██ ",
-      "███████ ███████ ███████ ██ ██  ██ █████  ",
-      "     ██ ██   ██ ██   ██ ██  ██ ██ ██  ██ ",
-      "███████ ██   ██ ██   ██ ██   ████ ██   ██",
-    },
-  },
+	nvdash = {
+		load_on_startup = true,
+		header = {
+			"███████ ██   ██  █████  ███    ██ ██   ██",
+			"██      ██   ██ ██   ██ ████   ██ ██  ██ ",
+			"███████ ███████ ███████ ██ ██  ██ █████  ",
+			"     ██ ██   ██ ██   ██ ██  ██ ██ ██  ██ ",
+			"███████ ██   ██ ██   ██ ██   ████ ██   ██",
+		},
+	},
 
-  cheatsheet = {
-    theme = "simple",
-  },
+	cheatsheet = {
+		theme = "simple",
+	},
 
-  telescope = {
-    style = "bordered",
-  },
+	telescope = {
+		style = "bordered",
+	},
 
-  statusline = {
-    overriden_modules = function()
-      local st_modules = require "nvchad_ui.statusline.default"
-      local abspath = vim.fn.expand "%p"
+	statusline = {
+		overriden_modules = function(modules)
+			-- for module indexes, see https://github.com/NvChad/ui/blob/47ef4046504e5562a3daf30db50b7d4a1552fcb5/lua/nvchad_ui/statusline/default.lua#L185
 
-      return {
-        fileInfo = function()
-          return st_modules.fileInfo() .. "%#St_file_txt#" .. abspath .. " "
-        end,
+			-- [2] : fileinfo
+			local st_modules = require("nvchad_ui.statusline.default")
+			local abspath = vim.fn.expand("%p")
+			modules[2] = (function()
+				return st_modules.fileInfo() .. "%#St_file_txt#" .. abspath .. " "
+			end)()
 
-        cursor_position = function()
-          return "%#StText# %l:%c "
-        end,
-      }
-    end,
-  },
+			-- [8] : cursor_position
+			modules[8] = (function()
+				return "%#StText# %l:%c "
+			end)()
+		end,
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
