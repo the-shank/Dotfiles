42d4b79ed3720f9e58ba5bc6a51cbb5626b0d5af
Shank <shashanksharma21@gmail.com>
2023-10-04 22:20:57 -0400

    nvim | change light theme to penumbra_light

--- neovim/.config/nvim/lua/custom/chadrc.lua
+++ neovim/.config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "everforest_light",
-	theme_toggle = { "everforest_light", "everforest" },
+	theme = "penumbra_light",
+	theme_toggle = { "penumbra_light", "everforest" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,
