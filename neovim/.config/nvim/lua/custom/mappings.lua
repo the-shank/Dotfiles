33b98d1a845cac75a37b69434ada3161df9cc238
Shank <shashanksharma21@gmail.com>
2023-10-27 01:08:44 -0400

    nvim | mapping to resume last telescope picker

--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -44,6 +44,7 @@ M.telescope = {
 		-- ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true ignore-case exclude=target/ exclude=.git/ exclude=.cache/ <CR>", "Find all" },
 		["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Document Symbols" },
 		["<leader>fS"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Workspace Symbols" },
+		["<leader>'"] = { "<cmd> Telescope resume <CR>", "Resume Picker" },
 	},
 }
 
