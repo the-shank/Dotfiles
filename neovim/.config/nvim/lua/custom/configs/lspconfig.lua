36aa5f56e608085ed9f0575eeccbc57e3d8cff24
Shank <shashanksharma21@gmail.com>
2023-10-31 19:01:12 -0400

    nvim | clangd lspconfig to fix the encoding offset error

--- neovim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+local servers = { "html", "cssls", "tsserver", "pyright" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
@@ -15,3 +15,13 @@ for _, lsp in ipairs(servers) do
 end
 
 -- lspconfig.pyright.setup { blabla}
+
+-- source: https://www.reddit.com/r/neovim/comments/12qbcua/comment/jgpqxsp
+lspconfig.clangd.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+	},
+})
