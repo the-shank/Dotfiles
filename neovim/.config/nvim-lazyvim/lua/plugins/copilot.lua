43cdd59e03b55d23a62859d6385ba70d844d792c
Shank <shashanksharma21@gmail.com>
2023-11-17 19:59:34 -0500

    nvim | add copilot

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/copilot.lua
@@ -0,0 +1,21 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = true, auto_trigger = true },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<esc>",
+    },
+  },
+}
