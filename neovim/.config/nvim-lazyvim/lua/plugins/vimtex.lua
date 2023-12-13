d8446fb764b0f7be4adce591cba5bca01eaf9a1e
Shank <shashanksharma21@gmail.com>
2023-12-13 14:52:14 -0500

    nvim | lazyvim | ignore some vimtex warnings

--- neovim/.config/nvim-lazyvim/lua/plugins/vimtex.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/vimtex.lua
@@ -3,5 +3,13 @@ return {
   lazy = false, -- lazy-loading will disable inverse search
   init = function()
     vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_log_ignore = {
+      "Underfull",
+      "Overfull",
+    }
+    vim.g.vimtex_quickfix_ignore_filters = {
+      "Underfull",
+      "Overfull",
+    }
   end,
 }
