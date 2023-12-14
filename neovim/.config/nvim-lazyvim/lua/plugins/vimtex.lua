e8180b5f6181f96292db36a9096722df050834ad
Shank <shashanksharma21@gmail.com>
2023-12-13 22:20:28 -0500

    nvim | lazyvim | enable -shell-escape with latexmk (required for minted)

--- neovim/.config/nvim-lazyvim/lua/plugins/vimtex.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/vimtex.lua
@@ -11,5 +11,14 @@ return {
       "Underfull",
       "Overfull",
     }
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        "-shell-escape",
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+      },
+    }
   end,
 }
