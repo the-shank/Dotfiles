4a610d813429235eea9d134bca696e32757d049b
Shank <shashanksharma21@gmail.com>
2024-01-16 23:33:53 -0500

    fish | abbr | procs light and dark

--- fish/.config/fish/sourced/abbreviations.fish
+++ fish/.config/fish/sourced/abbreviations.fish
@@ -228,7 +228,9 @@ abbr -a -g d120 "dpi.sh 120"
 abbr -a -g d114 "dpi.sh 114"
 abbr -a -g nf neofetch
 abbr -a -g jb "just build"
-abbr -a -g procs "procs --theme dark"
+abbr -a -g procs "procs --theme light"
+abbr -a -g procsl "procs --theme light"
+abbr -a -g procsd "procs --theme dark"
 
 # keyboard
 # - increase repeat rate
