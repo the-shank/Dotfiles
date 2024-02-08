b8eb58c008db5a44761c872e6872ebd0c11ab303
Shank <shashanksharma21@gmail.com>
2024-02-08 17:46:17 -0500

    nvim | enable luasnip

--- neovim/.config/nvim-lazyvim/lua/plugins/luasnip.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/luasnip.lua
@@ -1,5 +1,5 @@
 return {
   { "rafamadriz/friendly-snippets", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = true },
   { "saadparwaiz1/cmp_luasnip", enabled = false },
 }
