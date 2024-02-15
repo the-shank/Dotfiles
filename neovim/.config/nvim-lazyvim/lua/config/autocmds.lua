93649347c2e77629175de6d42c989a596f803363
Shank <shashanksharma21@gmail.com>
2024-02-15 12:11:29 -0500

    nvim | add descriptions to keymaps to make them searchable

--- neovim/.config/nvim-lazyvim/lua/config/autocmds.lua
+++ neovim/.config/nvim-lazyvim/lua/config/autocmds.lua
@@ -6,21 +6,17 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_shank_" .. name, { clear = true })
 end
 
--- which-key
-local wk = require("which-key")
-
 -- obisidian
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup("obsidian_open"),
   pattern = {
     "/home/shank/Sync/MyBrain/**.md",
-    "/home/shank/Downloads/tmp-vault/**.md",
   },
   callback = function()
-    vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<cr>")
-    vim.keymap.set("n", "<leader>on", ":ObsidianNew ")
-    vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<cr>")
-    vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<cr>")
+    vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<cr>", { desc = "ObsidianOpen" })
+    vim.keymap.set("n", "<leader>on", ":ObsidianNew ", { desc = "ObsidianNew" })
+    vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<cr>", { desc = "ObsidianSearch" })
+    vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<cr>", { desc = "ObsidianBacklinks" })
   end,
 })
 
