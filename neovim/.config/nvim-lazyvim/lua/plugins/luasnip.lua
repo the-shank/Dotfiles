b4e24d5591645971b6c563498e02dee63a32ee64
Shank <shashanksharma21@gmail.com>
2024-02-26 23:53:36 -0500

    nvim | fix <tab> key jumping around in insert mode

--- neovim/.config/nvim-lazyvim/lua/plugins/luasnip.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/luasnip.lua
@@ -1,5 +1,9 @@
 return {
   { "rafamadriz/friendly-snippets", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = true },
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = true,
+    opts = { history = false, region_check_events = "InsertEnter", delete_check_events = "TextChanged,InsertLeave" },
+  },
   { "saadparwaiz1/cmp_luasnip", enabled = false },
 }
