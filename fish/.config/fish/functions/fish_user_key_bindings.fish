fccda601c6917a6f7562c6fca10dc6be01fa94c1
Shank <shashanksharma21@gmail.com>
2024-03-23 02:35:18 -0400

    fish | set alt-backspace to delete word backward in vim mode as well

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,14 @@
-function fish_user_key_bindings -d "use Ctrl-f to accept suggestions in vi mode"
+function fish_user_key_bindings -d "user keybindings"
     fish_vi_key_bindings
+    # accept suggestion
     for mode in insert default visual
         bind -M $mode \cf forward-char
         bind -M $mode \ef nextd-or-forward-word
+    end
+    # delete word backword
+    for mode in insert default visual
+        # using "fish_key_reader", found that <alt-backspace> maps to "\e\x7F"
+        bind -M $mode \e\x7F backward-kill-word
     end
 end
 
