930c8df3d8c4432b25b7474e962c6860a87df614
Shank <shashanksharma21@gmail.com>
2024-06-30 11:33:19 -0400

    nvim | disable git-worktree and telescope related customization

--- neovim/.config/nvim-lazyvim/lua/plugins/git-worktree.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/git-worktree.lua
@@ -1,26 +1,26 @@
 return {
-  "telescope.nvim",
-  dependencies = {
-    "ThePrimeagen/git-worktree.nvim",
-    lazy = false,
-    config = function()
-      require("telescope").load_extension("git_worktree")
-    end,
-  },
-  keys = {
-    {
-      "<leader>gws",
-      function()
-        require("telescope").extensions.git_worktree.git_worktrees()
-      end,
-      desc = "git worktree search",
-    },
-    {
-      "<leader>gwc",
-      function()
-        require("telescope").extensions.git_worktree.create_git_worktree()
-      end,
-      desc = "git worktree create",
-    },
-  },
+  -- "telescope.nvim",
+  -- dependencies = {
+  --   "ThePrimeagen/git-worktree.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("telescope").load_extension("git_worktree")
+  --   end,
+  -- },
+  -- keys = {
+  --   {
+  --     "<leader>gws",
+  --     function()
+  --       require("telescope").extensions.git_worktree.git_worktrees()
+  --     end,
+  --     desc = "git worktree search",
+  --   },
+  --   {
+  --     "<leader>gwc",
+  --     function()
+  --       require("telescope").extensions.git_worktree.create_git_worktree()
+  --     end,
+  --     desc = "git worktree create",
+  --   },
+  -- },
 }
--- neovim/.config/nvim-lazyvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,102 +1,102 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  -- "telescope.nvim",
-  dependencies = {
-    {
-      "nvim-telescope/telescope-live-grep-args.nvim",
-      -- This will not install any breaking changes.
-      -- For major updates, this must be adjusted manually.
-      version = "^1.0.0",
-    },
-  },
-  keys = {
-    {
-      "<leader><space>",
-      function()
-        -- require("telescope.builtin").find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
-        require("telescope.builtin").find_files({
-          find_command = {
-            "fd",
-            "--ignore-case",
-            "--hidden",
-            "--no-ignore-vcs",
-            "--type=f",
-            "--exclude=.git/",
-            "--exclude=target/",
-            "--exclude=.venv/",
-          },
-        })
-      end,
-      desc = "telescope find files",
-    },
-    {
-      "<leader>/",
-      function()
-        require("telescope.builtin").live_grep()
-      end,
-      desc = "telescop grep",
-    },
-    {
-      "<leader>fg",
-      function()
-        require("telescope").extensions.live_grep_args.live_grep_args()
-      end,
-      desc = "telescope live grep args",
-    },
-    {
-      "<leader>sB",
-      function()
-        -- require("telescope").lsp_workspace_symbols()
-        require("telescope.builtin").lsp_workspace_symbols({
-          symbols = require("lazyvim.config").get_kind_filter(),
-        })
-      end,
-      desc = "Goto Symbol (workspace)",
-    },
-  },
-  config = function(_, opts)
-    local telescope = require("telescope")
-    telescope.setup(opts)
-    telescope.load_extension("live_grep_args")
-  end,
-  opts = function()
-    local actions = require("telescope.actions")
-    local lga_actions = require("telescope-live-grep-args.actions")
-    local picker_fraction = 0.95
-    return {
-      defaults = {
-        layout_strategy = "vertical",
-        layout_config = {
-          width = function(_, max_columns, _)
-            return math.floor(picker_fraction * max_columns)
-          end,
-          height = function(_, _, max_lines)
-            return math.floor(picker_fraction * max_lines)
-          end,
-          prompt_position = "bottom",
-        },
-      },
-      pickers = {
-        buffers = {
-          mappings = {
-            i = {
-              ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
-            },
-          },
-        },
-      },
-      extensions = {
-        live_grep_args = {
-          auto_quoting = true,
-          mappings = {
-            i = {
-              ["<C-k>"] = lga_actions.quote_prompt(),
-              ["<C-h>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
-              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-            },
-          },
-        },
-      },
-    }
-  end,
+  -- "nvim-telescope/telescope.nvim",
+  -- -- "telescope.nvim",
+  -- dependencies = {
+  --   {
+  --     "nvim-telescope/telescope-live-grep-args.nvim",
+  --     -- This will not install any breaking changes.
+  --     -- For major updates, this must be adjusted manually.
+  --     version = "^1.0.0",
+  --   },
+  -- },
+  -- keys = {
+  --   {
+  --     "<leader><space>",
+  --     function()
+  --       -- require("telescope.builtin").find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
+  --       require("telescope.builtin").find_files({
+  --         find_command = {
+  --           "fd",
+  --           "--ignore-case",
+  --           "--hidden",
+  --           "--no-ignore-vcs",
+  --           "--type=f",
+  --           "--exclude=.git/",
+  --           "--exclude=target/",
+  --           "--exclude=.venv/",
+  --         },
+  --       })
+  --     end,
+  --     desc = "telescope find files",
+  --   },
+  --   {
+  --     "<leader>/",
+  --     function()
+  --       require("telescope.builtin").live_grep()
+  --     end,
+  --     desc = "telescop grep",
+  --   },
+  --   {
+  --     "<leader>fg",
+  --     function()
+  --       require("telescope").extensions.live_grep_args.live_grep_args()
+  --     end,
+  --     desc = "telescope live grep args",
+  --   },
+  --   {
+  --     "<leader>sB",
+  --     function()
+  --       -- require("telescope").lsp_workspace_symbols()
+  --       require("telescope.builtin").lsp_workspace_symbols({
+  --         symbols = require("lazyvim.config").get_kind_filter(),
+  --       })
+  --     end,
+  --     desc = "Goto Symbol (workspace)",
+  --   },
+  -- },
+  -- config = function(_, opts)
+  --   local telescope = require("telescope")
+  --   telescope.setup(opts)
+  --   telescope.load_extension("live_grep_args")
+  -- end,
+  -- opts = function()
+  --   local actions = require("telescope.actions")
+  --   local lga_actions = require("telescope-live-grep-args.actions")
+  --   local picker_fraction = 0.95
+  --   return {
+  --     defaults = {
+  --       layout_strategy = "vertical",
+  --       layout_config = {
+  --         width = function(_, max_columns, _)
+  --           return math.floor(picker_fraction * max_columns)
+  --         end,
+  --         height = function(_, _, max_lines)
+  --           return math.floor(picker_fraction * max_lines)
+  --         end,
+  --         prompt_position = "bottom",
+  --       },
+  --     },
+  --     pickers = {
+  --       buffers = {
+  --         mappings = {
+  --           i = {
+  --             ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+  --           },
+  --         },
+  --       },
+  --     },
+  --     extensions = {
+  --       live_grep_args = {
+  --         auto_quoting = true,
+  --         mappings = {
+  --           i = {
+  --             ["<C-k>"] = lga_actions.quote_prompt(),
+  --             ["<C-h>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
+  --             ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+  --           },
+  --         },
+  --       },
+  --     },
+  --   }
+  -- end,
 }
