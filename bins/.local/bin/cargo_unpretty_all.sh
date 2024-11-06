6f06a17d837ad814da36f08dcfbb9f01a9194c69
Shank <shashanksharma21@gmail.com>
2024-11-06 12:43:44 -0500

    bins | cargo_unpretty_all | add option to unpretty only for lib target

--- bins/.local/bin/cargo_unpretty_all.sh
+++ bins/.local/bin/cargo_unpretty_all.sh
@@ -36,22 +36,62 @@ run() {
   bash -c "${runcmd}"
 }
 
+do_lib=false
+
+# Function to check if "--lib" is present in the arguments
+check_for_lib() {
+  for arg in "$@"; do
+    if [ "$arg" == "--lib" ]; then
+      echo "--lib argument found"
+      do_lib=true
+      return
+    fi
+  done
+  echo "--lib argument not found"
+  return
+}
+
+# Call the function with all the script arguments
+check_for_lib "$@"
+
 check_cargo_toml
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=normal >crate.01.normal 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=identified >crate.02.identified 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded >crate.03.expanded 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded,identified >crate.04.expanded.identified 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded,hygiene >crate.05.expanded.hygiene 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=ast-tree >crate.06.ast-tree 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=ast-tree,expanded >crate.07.ast-tree.expanded 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir >crate.08.hir 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir,identified >crate.09.hir.identified 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir,typed >crate.10.hir.typed 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir-tree >crate.11.hir-tree 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=thir-tree >crate.12.thir-tree 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=thir-flat >crate.13.thir-flat 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir -Z mir-opt-level=0 -Z mir-include-spans >crate.14.mir-built 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir-cfg -Z mir-opt-level=0 >crate.15.mir-built-cfg 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir -Z mir-include-spans >crate.16.mir 2>/dev/null"
-run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir-cfg >crate.17.mir-cfg 2>/dev/null"
+
+if [ "$do_lib" = true ]; then
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=normal >crate.01.normal 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=identified >crate.02.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=expanded >crate.03.expanded 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=expanded,identified >crate.04.expanded.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=expanded,hygiene >crate.05.expanded.hygiene 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=ast-tree >crate.06.ast-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=ast-tree,expanded >crate.07.ast-tree.expanded 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=hir >crate.08.hir 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=hir,identified >crate.09.hir.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=hir,typed >crate.10.hir.typed 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=hir-tree >crate.11.hir-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=thir-tree >crate.12.thir-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=thir-flat >crate.13.thir-flat 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=mir -Z mir-opt-level=0 -Z mir-include-spans >crate.14.mir-built 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=mir-cfg -Z mir-opt-level=0 >crate.15.mir-built-cfg 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=mir -Z mir-include-spans >crate.16.mir 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc --lib -- -Z unpretty=mir-cfg >crate.17.mir-cfg 2>/dev/null"
 # run "cargo clean && cargo rustc -- -Z unpretty=stable-mir >crate.16.stable-mir 2>/dev/null"
+else
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=normal >crate.01.normal 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=identified >crate.02.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded >crate.03.expanded 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded,identified >crate.04.expanded.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=expanded,hygiene >crate.05.expanded.hygiene 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=ast-tree >crate.06.ast-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=ast-tree,expanded >crate.07.ast-tree.expanded 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir >crate.08.hir 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir,identified >crate.09.hir.identified 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir,typed >crate.10.hir.typed 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=hir-tree >crate.11.hir-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=thir-tree >crate.12.thir-tree 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=thir-flat >crate.13.thir-flat 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir -Z mir-opt-level=0 -Z mir-include-spans >crate.14.mir-built 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir-cfg -Z mir-opt-level=0 >crate.15.mir-built-cfg 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir -Z mir-include-spans >crate.16.mir 2>/dev/null"
+  run "cargo clean && cargo +nightly rustc -- -Z unpretty=mir-cfg >crate.17.mir-cfg 2>/dev/null"
+# run "cargo clean && cargo rustc -- -Z unpretty=stable-mir >crate.16.stable-mir 2>/dev/null"
+fi
