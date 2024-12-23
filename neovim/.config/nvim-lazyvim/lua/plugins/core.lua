a766d16ac95213eb580deecbfa1a5ce8e34c60a2
Shank <shashanksharma21@gmail.com>
2024-12-23 01:33:00 -0500

    nvim | add more kind_filters for symbols for rust (static and constant)

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/core.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      kind_filter = {
+        rust = {
+          "Class",
+          "Constructor",
+          "Enum",
+          "Field",
+          "Function",
+          "Interface",
+          "Method",
+          "Module",
+          "Namespace",
+          "Package",
+          "Property",
+          "Struct",
+          "Trait",
+          "Constant", -- For static variables
+          "Static", -- For static variables
+        },
+      },
+    },
+  },
+}
