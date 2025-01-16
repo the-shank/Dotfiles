26e86827144e1ce400ee00d7f962b28f655026cb
Shank <shashanksharma21@gmail.com>
2025-01-16 00:00:48 -0500

    nvim | tokyonight | disable italics

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    styles = {
+      comments = {
+        italic = false,
+      },
+      keywords = {
+        italic = false,
+      },
+    },
+  },
+}
