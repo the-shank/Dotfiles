d7dd4870d25adb084d6b12df198a3695c203ea75
Shank <shashanksharma21@gmail.com>
2025-01-23 23:31:56 -0500

    nvim | disable animations

--- neovim/.config/nvim-lazyvim/lua/config/options.lua
+++ neovim/.config/nvim-lazyvim/lua/config/options.lua
@@ -28,3 +28,6 @@ vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_foreground = "mix"
 vim.g.gruvbox_material_better_performance = 1
+
+-- disable lazyvim animations
+vim.g.snacks_animate = false
