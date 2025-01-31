f6fc05ddb57aa6a3991f5b054e0a9c7a78113db1
Shank <shashanksharma21@gmail.com>
2025-01-31 18:17:54 -0500

    copyq | update config

--- neovim/.config/nvim-lazyvim/lua/config/options.lua
+++ neovim/.config/nvim-lazyvim/lua/config/options.lua
@@ -26,7 +26,7 @@ vim.opt.conceallevel = 1
 vim.g.gruvbox_material_enable_italic = false
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "mix"
+vim.g.gruvbox_material_foreground = "material"
 vim.g.gruvbox_material_better_performance = 1
 
 -- disable lazyvim animations
