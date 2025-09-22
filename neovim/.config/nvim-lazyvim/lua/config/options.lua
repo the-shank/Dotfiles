aad1ebc0de4556a107563b6397866d61e5609fde
Shank <shashanksharma21@gmail.com>
2025-09-22 11:36:51 -0400

    tmp

--- neovim/.config/nvim-lazyvim/lua/config/options.lua
+++ neovim/.config/nvim-lazyvim/lua/config/options.lua
@@ -26,7 +26,7 @@ vim.opt.conceallevel = 1
 vim.g.gruvbox_material_enable_italic = false
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_inlay_hints_background = "dimmed"
-vim.g.gruvbox_material_background = "medium"
+-- vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_foreground = "mix"
 vim.g.gruvbox_material_better_performance = 1
 
