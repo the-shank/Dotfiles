f31d9bdb4857010e5c27aa440db8e7774fdca0ae
Shank <shashanksharma21@gmail.com>
2025-11-30 14:05:40 -0500

    nvim | disable certain markdown warnings for the newer linter

--- neovim/.config/nvim-lazyvim/lua/plugins/lint.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/lint.lua
@@ -6,6 +6,9 @@ return {
         markdownlint = {
           args = { "--disable", "MD007", "MD013", "MD034", "MD041", "--" },
         },
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fn.expand("../config/markdownlint.json"), "--" },
+        },
       },
     },
   },
