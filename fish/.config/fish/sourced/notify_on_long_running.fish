cd479d4817e571dd02f03b3a787a19d451aa95a8
Shank <shashanksharma21@gmail.com>
2025-12-02 01:19:58 -0500

    fish | ignore some programs regarding the long running task notification

--- fish/.config/fish/sourced/notify_on_long_running.fish
+++ fish/.config/fish/sourced/notify_on_long_running.fish
@@ -1,6 +1,27 @@
 function notify_on_long_running --on-event fish_postexec
     set -l threshold 5000
 
+    set -l ignored_cmds \
+        # text editors
+        nvim vim vi nano helix hx \
+        # pagers & documentation
+        man less \
+        # remote & shells
+        ssh telnet \
+        # system monitors
+        top htop btop nvtop iotop glances \
+        # multiplexers
+        tmux zellij screen \
+        # file managers
+        ranger yazi nnn lf mc \
+        # interactive
+        lazygit lazydocker python ipython fzf
+    set -l cmd_clean (string replace -r '^sudo\s+' '' -- $argv[1])
+    set -l cmd_name (string split -f1 " " -- $cmd_clean)
+    if contains -- $cmd_name $ignored_cmds
+        return
+    end
+
     if test $CMD_DURATION -gt $threshold
         set -l status_msg Success
         if test $status -ne 0
