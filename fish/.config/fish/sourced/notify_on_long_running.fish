0c248d33ff2b682329c9d9d321b028f08160c116
Shank <shashanksharma21@gmail.com>
2025-12-06 20:57:06 -0500

    fish | add ldconfig to list of long running programs to ignore

--- fish/.config/fish/sourced/notify_on_long_running.fish
+++ fish/.config/fish/sourced/notify_on_long_running.fish
@@ -17,7 +17,7 @@ function notify_on_long_running --on-event fish_postexec
         # file managers
         ranger y yazi nnn lf mc \
         # interactive
-        lazygit lazydocker python ipython fzf git rga-fzf
+        lazygit lazydocker python ipython fzf git rga-fzf ldconfig
     set -l cmd_clean (string replace -r '^sudo\s+' '' -- $argv[1])
     set -l cmd_name (string split -f1 " " -- $cmd_clean)
     if contains -- $cmd_name $ignored_bins
