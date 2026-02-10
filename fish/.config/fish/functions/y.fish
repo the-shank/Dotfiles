2c9ccb316bab481c59c930374ab0a3719f9063cf
Shank <shashanksharma21@gmail.com>
2026-02-10 13:05:54 -0500

    fish | fix y alias

--- fish/.config/fish/functions/y.fish
+++ fish/.config/fish/functions/y.fish
@@ -1,7 +1,7 @@
 # yazi
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
+    command yazi $argv --cwd-file="$tmp" </dev/tty
     if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
         builtin cd -- "$cwd"
     end
